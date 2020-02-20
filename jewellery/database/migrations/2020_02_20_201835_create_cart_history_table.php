5902b3aefa8b3d3131b6f6cb90b76fa033d54edf
htetmyattun <54614442+htetmyattun@users.noreply.github.com>
2020-02-20 12:21:28 -0800

    Update Migration

---
+++ jewellery/database/migrations/2020_02_20_201835_create_cart_history_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('cart_history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->nullable();;
+            $table->bigInteger('product_id')->unsigned()->nullable();;
+            $table->integer('quantity')->nullable();
+            $table->bigInteger('order_id')->unsigned()->nullable();;
+            $table->boolean('checkout')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('order')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_history');
+    }
+}
