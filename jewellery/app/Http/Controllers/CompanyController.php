12a28dc50e13eae3bfa5c95a63fe95c218df8c43
Zeyar Naing <ZeYarNaing0000@gmail.com>
2020-03-03 12:51:28 +0630

    company profile

--- jewellery/app/Http/Controllers/CompanyController.php
+++ jewellery/app/Http/Controllers/CompanyController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Conversation;
+use App\Models\Company;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -24,7 +27,14 @@ class CompanyController extends Controller
      */
     public function profile()
     {
-        return view('client.pages.company_profile');
+        $company_id = auth()->user()->id;
+        $company = Company::where('id', '=', $company_id)->first();
+        $products =  Product::leftJoin('gallery', 'product.id', '=', 'gallery.product_id')
+      	->where('product.company_id', '=', $company_id)
+      	->where('gallery.type', '=', 'typemain')->select('product.*', 'gallery.url')
+      	->orderBy('product.created_at','DESC')
+      	->paginate(12);
+        return view('company.pages.company_profile', ['company' => $company, 'products' => $products]);
     }
     public function chat(Request $request)
     {
--- jewellery/resources/views/payment/check_out.blade.php
+++ jewellery/resources/views/payment/check_out.blade.php
@@ -1,7 +1,21 @@
 @extends('client.layouts.default')
 @section('title','Company Profile')
 @section('content')
-
+<!--Mandatory Attributes:
+Form 'id': Used to identify the form that will be submitted to the backend.
+Form 'action': The address of the backend code that will submit payment request to 2c2p Payment Gateway.
+Form 'method': Should be set to 'POST' method.
+-->
+		<!--Remove attribute 'name' from the following fields: Card number, Cvv, Expiry Month and Year, to prevent
+				these data from being sent over to backend side -->
+		<!--Mandatory Attributes:
+				Input 'data-encrypt':     this attribute must be added for the following fields: Card number, Cvv, Expiry Month and Year.
+				values:	data-encrypt="cardnumber"
+								data-encrypt="month"
+								data-encrypt="year"
+								data-encrypt="cvv"
+				Input 'type': set type to 'text' for card number, month and year fields, and set type 'password' for cvv field.
+		-->
 <main>
     <!-- breadcrumb area start -->
     <div class="breadcrumb-area">
@@ -26,31 +40,36 @@
         </div>
     </div>
     <!-- breadcrumb area end -->
-		<div>
-			<!--Mandatory Attributes:
-			Form 'id': Used to identify the form that will be submitted to the backend.
-			Form 'action': The address of the backend code that will submit payment request to 2c2p Payment Gateway.
-			Form 'method': Should be set to 'POST' method.
-		-->
-				<form id="2c2p-payment-form" action="{{url('check_out')}}" method="POST">
-					<!--Remove attribute 'name' from the following fields: Card number, Cvv, Expiry Month and Year, to prevent
-							these data from being sent over to backend side -->
-					<!--Mandatory Attributes:
-							Input 'data-encrypt':     this attribute must be added for the following fields: Card number, Cvv, Expiry Month and Year.
-							values:	data-encrypt="cardnumber"
-											data-encrypt="month"
-											data-encrypt="year"
-											data-encrypt="cvv"
-							Input 'type': set type to 'text' for card number, month and year fields, and set type 'password' for cvv field.
-					-->
-				<input type="text" data-encrypt="cardholderName" maxlength="50" placeholder="Name"><br/>
-				<input type="text" data-encrypt="cardnumber" maxlength="16" placeholder="Credit Card Number"><br/>
-				<input type="text" data-encrypt="month" maxlength="2" placeholder="MM"><br/>
-				<input type="text" data-encrypt="year" maxlength="4" placeholder="YYYY"><br/>
-				<input type="password" data-encrypt="cvv" maxlength="4" autocomplete="off" placeholder="CVV2/CVC2"><br/>
-				<input type="submit" value="Submit">
-				</form>
+		<div class="col-lg-6">
+      <div class="container" align="center">
+        <div class="login-reg-form-wrap sign-up-form">
+  				<h5 class="Royal_Crescent_Bold">Payment Form</h5>
+  				<br>
+  				<form id="2c2p-payment-form" action="{{url('check_out')}}" method="POST">
+  					<div class="single-input-item">
+  						<input type="text" data-encrypt="cardholderName" maxlength="50" placeholder="Name"><br/>
+  					</div>
+  					<div class="single-input-item">
+  						<input type="text" data-encrypt="cardnumber" maxlength="16" placeholder="Credit Card Number"><br/>
+  					</div>
+  					<div class="single-input-item">
+  						<input type="text" data-encrypt="month" maxlength="2" placeholder="MM"><br/>
+  					</div>
+  					<div class="single-input-item">
+  						<input type="text" data-encrypt="year" maxlength="4" placeholder="YYYY"><br/>
+  					</div>
+  					<div class="single-input-item">
+  						<input type="password" data-encrypt="cvv" maxlength="4" autocomplete="off" placeholder="CVV2/CVC2"><br/>
+  					</div>
+  					<div class="single-input-item">
+  						<button class="btn btn-sqr" type="submit">Submit</button>
+  					</div>
+  				</form>
+  			</div>
+  		 </div>
+      </div>
 
+		<div>
 				<!--Importing 2c2p JSLibrary-->
 				<script type="text/javascript" src="https://demo2.2c2p.com/2C2PFrontEnd/SecurePayment/api/my2c2p.1.6.9.min.js"></script>
 				<script type="text/javascript">
